c7c9b8ea9437247f0838e4fb1c98a0a8dd92692f
Hayden Cosc <haydenevans828@gmail.com>
2024-03-26 14:37:35 +1300

    First training

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Attempting to use YOLOV8 with deep-darts annotated images
+Training a YOLOV8 model on dart board images anottated by [DeepDarts](https://github.com/wmcnally/deep-darts/tree/master)
--- trainv8.py
+++ trainv8.py
@@ -8,3 +8,5 @@ model = YOLO('models/yolov8n.pt')  # load a pretrained model (recommended for tr
 
 # Train the model
 results = model.train(data='/csse/users/hev23/Documents/Computer Vision/Project/deeper_darts/datasets/val/data_custom.yaml', epochs=100, batch=8, imgsz=800, verbose=True)
+#this line didn't work, so run from terminal instead:
+# yolo task=detect mode=train epochs=100 data=data_custom.yaml model=models/yolov8n.pt imgsz=800 verbose=True
