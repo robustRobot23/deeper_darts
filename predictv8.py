9d6cbf90ef12897f34cdbc7c0873c0b2204008e2
Hayden Cosc <haydenevans828@gmail.com>
2024-04-02 16:18:13 +1300

    second training in progress

---
+++ predictv8.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+from dataset.annotate import draw, get_dart_scores
+
+
+def bboxes_to_xy(bboxes, max_darts=3):
+    xy = np.zeros((4 + max_darts, 3), dtype=np.float32)
+    for cls in range(5):
+        if cls == 0:
+            dart_xys = bboxes[bboxes[:, 4] == 0, :2][:max_darts]
+            xy[4:4 + len(dart_xys), :2] = dart_xys
+        else:
+            cal = bboxes[bboxes[:, 4] == cls, :2]
+            if len(cal):
+                xy[cls - 1, :2] = cal[0]
+    xy[(xy[:, 0] > 0) & (xy[:, 1] > 0), -1] = 1
+    if np.sum(xy[:4, -1]) == 4:
+        return xy
+    else:
+        xy = est_cal_pts(xy)
+    return xy
+
+
+def est_cal_pts(xy):
+    missing_idx = np.where(xy[:4, -1] == 0)[0]
+    if len(missing_idx) == 1:
+        if missing_idx[0] <= 1:
+            center = np.mean(xy[2:4, :2], axis=0)
+            xy[:, :2] -= center
+            if missing_idx[0] == 0:
+                xy[0, 0] = -xy[1, 0]
+                xy[0, 1] = -xy[1, 1]
+                xy[0, 2] = 1
+            else:
+                xy[1, 0] = -xy[0, 0]
+                xy[1, 1] = -xy[0, 1]
+                xy[1, 2] = 1
+            xy[:, :2] += center
+        else:
+            center = np.mean(xy[:2, :2], axis=0)
+            xy[:, :2] -= center
+            if missing_idx[0] == 2:
+                xy[2, 0] = -xy[3, 0]
+                xy[2, 1] = -xy[3, 1]
+                xy[2, 2] = 1
+            else:
+                xy[3, 0] = -xy[2, 0]
+                xy[3, 1] = -xy[2, 1]
+                xy[3, 2] = 1
+            xy[:, :2] += center
+    else:
+        # TODO: if len(missing_idx) > 1
+        print('Missed more than 1 calibration point')
+    return xy
+
+
+
+if __name__ == '__main__':
+    from ultralytics import YOLO
+    image_path = ''
+
+    model = YOLO('models/yolov8n.pt')
+    results = model(image_path)
+    for result in results:
+        boxes = result.boxes
+        xy = bboxes_to_xy(boxes)
+        results.show() #display results to screen
+
--- trainv8.py
+++ trainv8.py
@@ -2,11 +2,11 @@ from ultralytics import YOLO
 
 # Load a model
 
-model = YOLO('models/yolov8n.pt')  # load a pretrained model (recommended for training)
+# model = YOLO('models/yolov8n.pt')  # load a pretrained model (recommended for training)
 
 # model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
 # Train the model
-results = model.train(data='/csse/users/hev23/Documents/Computer Vision/Project/deeper_darts/datasets/val/data_custom.yaml', epochs=100, batch=8, imgsz=800, verbose=True)
+# results = model.train(data='/csse/users/hev23/Documents/Computer Vision/Project/deeper_darts/datasets/val/data_custom.yaml', epochs=100, batch=8, imgsz=800, verbose=True)
 #this line didn't work, so run from terminal instead:
-# yolo task=detect mode=train epochs=100 data=data_custom.yaml model=models/yolov8n.pt imgsz=800 verbose=True
+# yolo task=detect mode=train epochs=100 data=data_custom.yaml model=models/yolov8n.pt imgsz=800 verbose=True name=SecondRun batch=8
