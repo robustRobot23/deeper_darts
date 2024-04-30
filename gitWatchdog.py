4e73f130c0b03227be1dc73f0bf2612376c6b6f8
Hayden Cosc <haydenevans828@gmail.com>
2024-04-30 15:49:50 +1200

    Automatic commit by watcher script

--- gitWatchdog.py
+++ gitWatchdog.py
@@ -1,11 +1,5 @@
 import time
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-import subprocess
 from git import Repo
-
-# Define the directory to monitor
-directory_to_watch = 'runs/detect/DeeperDarts'
 
 # Define the Git repository path
 repo_path = ''
@@ -20,12 +14,6 @@ def push_changes():
         print("Changes pushed to repository successfully")
     except Exception as e:
         print(f"Error pushing changes to repository: {e}")
-
-# Define a class to handle file system events
-class Watcher(FileSystemEventHandler):
-    def on_any_event(self, event):
-        print(f"Detected change: {event.event_type} - {event.src_path}")
-        push_changes()
 
 # Start monitoring the directory for changes
 if __name__ == "__main__":
