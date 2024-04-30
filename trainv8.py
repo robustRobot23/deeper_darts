7f6c15cee1391ccc7768384c55d770a26ca11f75
Hayden Cosc <haydenevans828@gmail.com>
2024-04-30 15:36:41 +1200

    training

--- trainv8.py
+++ trainv8.py
@@ -10,3 +10,5 @@ from ultralytics import YOLO
 # results = model.train(data='/csse/users/hev23/Documents/Computer Vision/Project/deeper_darts/datasets/val/data_custom.yaml', epochs=100, batch=8, imgsz=800, verbose=True)
 #this line didn't work, so run from terminal instead:
 # yolo task=detect mode=train epochs=100 data=data_custom.yaml model=models/yolov8n.pt imgsz=800 verbose=True name=SecondRun batch=8
+
+# yolo task=detect mode=train epochs=100 data=data_custom.yaml model=models/yolov8n.pt imgsz=800 verbose=True name=DeeperDarts batch=8 iou=0.3 cos_lr=True lr0=1E-3 augment=True save_conf=True plots=True
