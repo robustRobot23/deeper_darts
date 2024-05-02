1d5a82cb2e4fc23741ee8ccef22120d46b125170
Hayden Cosc <haydenevans828@gmail.com>
2024-05-02 17:24:57 +1200

    Automatic commit by watcher script

--- trainv8.py
+++ trainv8.py
@@ -12,3 +12,5 @@ from ultralytics import YOLO
 # yolo task=detect mode=train epochs=100 data=data_custom.yaml model=models/yolov8n.pt imgsz=800 verbose=True name=SecondRun batch=8
 
 # yolo task=detect mode=train epochs=100 data=data_custom.yaml model=models/yolov8n.pt imgsz=800 verbose=True name=DeeperDarts batch=8 iou=0.3 cos_lr=True lr0=1E-3 augment=True save_conf=True plots=True
+# yolo task=detect mode=train epochs=100 data=data_custom.yaml model=models/yolov8n.pt imgsz=800 verbose=True name=DeeperDarts batch=8 iou=0.3 cos_lr=True lr0=1E-3 optimizer=Adam augment=True save_conf=True plots=True 
+# yolo task=detect mode=train epochs=100 data=data_custom.yaml model=models/yolov8n.pt imgsz=800 verbose=True project=DeeperDarts name=droput_0.1 batch=8 iou=0.3 cos_lr=True lr0=1E-3 optimizer=Adam augment=True save_conf=True plots=True dropout=0.1
