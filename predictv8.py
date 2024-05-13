606266dd14090e74b3791c49703b2eee7e2752ac
Hayden Cosc <haydenevans828@gmail.com>
2024-05-13 12:53:28 +1200

    Automatic commit by watcher script

--- predictv8.py
+++ predictv8.py
@@ -129,17 +129,8 @@ def get_label_xy(image_name, folder_path, max_darts=3):
     label_xy[(label_xy[:, 0] > 0) & (label_xy[:, 1] > 0), -1] = 1
     return label_xy
 
-if __name__ == '__main__':
-    from ultralytics import YOLO
-    print("imported yolo")
 
-    from yacs.config import CfgNode as CN
-    import os.path as osp
-
-    #load configuration
-    cfg = CN(new_allowed=True)
-    cfg.merge_from_file('configs/deepdarts_d1.yaml')
-    
+def predict(model_directory):
     #path to test images
     image_folder_path = 'datasets/test/images/d1_03_31_2020'
 
@@ -148,28 +139,16 @@ if __name__ == '__main__':
 
     # Directory to search for weights
     results_directory = 'DeeperDarts'
-
-    # Get list of directories in the specified directory
-    results_directories = [d for d in os.listdir(results_directory) if os.path.isdir(os.path.join(results_directory, d))]
-
-    # Sort directories by modification time (most recent first)
-    results_directories.sort(key=lambda x: os.path.getmtime(os.path.join(results_directory, x)), reverse=True)
-
-    # Get the most recent directory
-    if results_directories:
-        most_recent_directory = results_directories[0]
-        print("Most recent directory:", most_recent_directory)
-    else:
-        print("No directories found in", results_directory)
     
-    best_weights_path = f'DeeperDarts/{most_recent_directory}/weights/best.pt'
+    best_weights_path = f'DeeperDarts/{model_directory}/weights/best.pt'
 
+    print(f"Loading {best_weights_path}")
     #load model
     model = YOLO(best_weights_path)
     errors = []
     no_error_total = 0
 
-    recent_results_img_dir = f"datasets/test/{most_recent_directory}"
+    recent_results_img_dir = f"datasets/test/{model_directory}"
     os.makedirs(recent_results_img_dir, exist_ok=True)
 
     labeled_img_dir = f"{recent_results_img_dir}/scored_images"
@@ -178,22 +157,31 @@ if __name__ == '__main__':
     predicted_img_dir = f"{recent_results_img_dir}/predicted_images"
     os.makedirs(predicted_img_dir, exist_ok=True)
 
+    log = logging.getLogger()
+    for hdlr in log.handlers[:]:
+        log.removeHandler(hdlr)
+
     # Set up logging
-    log_dir = f"test_logs/{most_recent_directory}"
+    log_dir = f"test_logs/{model_directory}"
     os.makedirs(log_dir, exist_ok=True)
     log_file = f"{log_dir}/processing_output.log"
-    logging.basicConfig(filename=log_file, filemode='w', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-    start_time = time.time_ns()
+    fileh = logging.FileHandler(log_file, 'a')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    fileh.setFormatter(formatter)
+    log.addHandler(fileh)
+    print(f"Predicting {best_weights_path}")
+    speeds = []
+    
     for i in range(len(images)):
         image = images[i]
         image_name = images[i].split('/')[-1]
         # print(f"Processing {i}th image: '{image_name}'")
 
-        result = model.predict(image)[0]
-        result.save(f"{predicted_img_dir}/{image_name}.JPG")
-        boxes = result.boxes        
-
+        results = model.predict(image)
+        image_result = results[0]
+        image_result.save(f"{predicted_img_dir}/{image_name}.JPG")
+        boxes = image_result.boxes  
+        speeds.append(image_result.speed['inference'])    
         xy = bboxes_to_xy(boxes) 
         xy = xy[xy[:, -1] == 1] #remove any empty rows
         predicted_score = get_dart_scores(xy,cfg, numeric=False)
@@ -218,18 +206,19 @@ if __name__ == '__main__':
         img = draw(cv2.cvtColor(img, cv2.COLOR_RGB2BGR), xy[:, :2], cfg, circles=False, score=True)
         cv2.imwrite(osp.join(labeled_img_dir, image_name), img)
 
-    total_inference_time = time.time_ns() - start_time
-    avg_inference_time_ms = round((total_inference_time/len(images)) / 1e6, 2)
+
+    avg_inference_time_ms = round(sum(speeds)/len(speeds), 2)
+
     abs_errors = map(abs, errors)
 
     avg_abs_error = round(sum(abs_errors)/len(errors),1)
     avg_error = round(sum(errors)/len(errors),)
     PCS = round((100/len(errors))*no_error_total,1)
 
-    test_name = most_recent_directory
+    test_name = model_directory
 
     #read training results.csv
-    with open(f"{results_directory}/{most_recent_directory}/results.csv") as f:
+    with open(f"{results_directory}/{model_directory}/results.csv") as f:
         epochs = 0
         reader = csv.reader(f)
         for row in reader:
@@ -244,3 +233,19 @@ if __name__ == '__main__':
     print(f"Average error: {sum(errors)/len(errors)}")
     print(f"PCS: {round((100/len(errors))*no_error_total,1)}%")
     print(f"Estimated Calibration Points {est_cal_pts_cnt} times")
+
+if __name__ == '__main__':
+    from ultralytics import YOLO
+    print("imported yolo")
+
+    from yacs.config import CfgNode as CN
+    import os.path as osp
+
+    #load configuration
+    cfg = CN(new_allowed=True)
+    cfg.merge_from_file('configs/deepdarts_d1.yaml')
+
+    model_directories = ["droput_0.13","adam_dropout_0.1","adam_dropout_0.3","adam_dropout_0.4", "SGD"]
+    
+    for model_directory in model_directories:
+        predict(model_directory)
--- train_varients.sh
+++ train_varients.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # test_names=("adam_dropout_0.1" "adam_dropout_0.3" "adam_dropout_0.4" "SGD" "RMSProp" "RAdam")
-# dropouts=(0.1 0.3 0.4 0 0 0)
-# optimizers=("Adam" "Adam" "Adam" "SGD" "RMSProp" "RAdam")
-test_names=("RMSProp" "RAdam")
-dropouts=(0 0)
-optimizers=("RMSProp" "RAdam")
 
+test_names=("Scale_1" "Shear_20" "Rot_15" "Scale_0.7" "Shear_30" "Rot_25")
+scales=(1 0 0 0.7 0)
+shears=(0 20 0 0 30 0)
+rotations=(0 0 15 0 0 25)
 for i in "${!test_names[@]}"; do
     test_name="${test_names[i]}"
-    dropout="${dropouts[i]}"
-    optimizer="${optimizers[i]}"
+    scale="${scales[i]}"
+    shear="${shears[i]}"
+    rotation="${rotations[i]}"
     echo "Running test" 
-    yolo task=detect mode=train epochs=100 data=data_custom.yaml model=models/yolov8n.pt imgsz=800 verbose=True project=DeeperDarts name=$test_name batch=8 iou=0.3 cos_lr=True lr0=1E-3 optimizer=$optimizer augment=True save_conf=True plots=True dropout=$dropout
+    yolo task=detect mode=train epochs=100 data=data_custom.yaml model=models/yolov8n.pt imgsz=800 verbose=False project=DeeperDarts name=$test_name batch=8 iou=0.3 cos_lr=True lr0=1E-2 optimizer=SGD augment=True save_conf=True plots=True fliplr=0 dropout=0 scale=$scale shear=$shear degrees=$rotation
 
 done
