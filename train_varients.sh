74d8559e68e242d60713068c723ffc520699b06e
Hayden Evans <hev23@uclive.ac.nz>
2024-05-17 21:14:39 +1200

    training results

--- train_varients.sh
+++ train_varients.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# test_names=("adam_dropout_0.1" "adam_dropout_0.3" "adam_dropout_0.4" "SGD" "RMSProp" "RAdam")
 
-test_names=("Rot_15" "Scale_0.7" "Shear_30" "Rot_25")
-scales=(0 0.7 0 0)
-shears=(0 0 30 0)
+test_names=("Shear_30_Scale_1_Rot_15" "Shear_30_Scale_1_Rot_8")
 rotations=(15 0 0 25)
 for i in "${!test_names[@]}"; do
     test_name="${test_names[i]}"
-    scale="${scales[i]}"
-    shear="${shears[i]}"
     rotation="${rotations[i]}"
     echo "Running test" 
-    yolo task=detect mode=train epochs=100 data=data_custom.yaml model=models/yolov8n.pt imgsz=800 verbose=False project=DeeperDarts name=$test_name batch=8 iou=0.3 cos_lr=True lr0=1E-2 optimizer=SGD augment=True save_conf=True plots=True fliplr=0 dropout=0 scale=$scale shear=$shear degrees=$rotation
+    yolo task=detect mode=train epochs=100 data=data_custom.yaml model=models/yolov8n.pt imgsz=800 verbose=False project=DeeperDarts name=$test_name batch=8 iou=0.3 cos_lr=True lr0=1E-2 optimizer=SGD augment=True save_conf=True plots=True fliplr=0 dropout=0 scale=1 shear=30 degrees=$rotation
 
 done
+
+
